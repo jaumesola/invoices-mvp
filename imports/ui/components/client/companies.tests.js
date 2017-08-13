0c2fa1d49b392f0d05ca654b50e1f9132e3e7a91
Jaume Sola <ask@jaumesola.com>
2017-08-13 22:44:39 +0200

    minimal dummy mocha test

---
+++ imports/ui/components/client/companies.tests.js
@@ -0,0 +1,77 @@
+import { Factory } from 'meteor/dburles:factory';
+import { chai } from 'meteor/practicalmeteor:chai';
+import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
+
+import { withRenderedTemplate } from './test-helpers.js';
+//import '/client/companies-client.js';
+
+describe('Companies', function () {
+
+/*	
+  beforeEach(function () {
+    Template.registerHelper('_', key => key);
+  });
+
+  afterEach(function () {
+    Template.deregisterHelper('_');
+  });
+
+*/
+	  it('is true', function () { chai.assert.isTrue(true); });
+	  //it('is true - wrong', function () { chai.assert.isTrue(false); });
+	  
+	  
+/*
+	  it('renders H2 title', function () {
+		  //const doc = Factory.build('doc');
+		  withRenderedTemplate('companies', {}, (el) => {
+			  chai.assert.isTrue(true);
+			  //chai.assert.equal($(el).find('h2').val(), "Companies List");
+		  });
+	  });
+*/
+
+/*
+  it('renders correctly with simple data', function () {
+    const todo = Factory.build('todo', { checked: false });
+    const data = {
+      todo: Todos._transform(todo),
+      onEditingChange: () => 0,
+    };
+
+    withRenderedTemplate('Todos_item', data, (el) => {
+      chai.assert.equal($(el).find('input[type=text]').val(), todo.text);
+      chai.assert.equal($(el).find('.list-item.checked').length, 0);
+      chai.assert.equal($(el).find('.list-item.editing').length, 0);
+    });
+  });
+
+  it('renders correctly when checked', function () {
+    const todo = Factory.build('todo', { checked: true });
+    const data = {
+      todo: Todos._transform(todo),
+      onEditingChange: () => 0,
+    };
+
+    withRenderedTemplate('Todos_item', data, (el) => {
+      chai.assert.equal($(el).find('input[type=text]').val(), todo.text);
+      chai.assert.equal($(el).find('.list-item.checked').length, 1);
+    });
+  });
+
+  it('renders correctly when editing', function () {
+    const todo = Factory.build('todo');
+    const data = {
+      todo: Todos._transform(todo),
+      editing: true,
+      onEditingChange: () => 0,
+    };
+
+    withRenderedTemplate('Todos_item', data, (el) => {
+      chai.assert.equal($(el).find('input[type=text]').val(), todo.text);
+      chai.assert.equal($(el).find('.list-item.editing').length, 1);
+    });
+  });
+  */
+});
