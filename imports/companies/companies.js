17ad3c708178df8456a1788c5467d719f7a78746
Jaume Sola <ask@jaumesola.com>
2017-08-19 09:19:20 +0200

    Add basic validations to the Company model

--- imports/companies/companies.js
+++ imports/companies/companies.js
@@ -4,13 +4,28 @@ import { Class as Model } from 'meteor/jagi:astronomy';
 Companies = new Mongo.Collection('companies');
 
 Company = Model.create({
-  name: 'Company',
-  collection: Companies,
-  fields: {
-    TaxId: String,
-    Name: String,
-    Rating: Number
-  }
+    name: 'Company',
+    collection: Companies,
+    fields: {
+        TaxId: {
+            type: String,
+            validators: [
+                { type: 'minLength', param: 5 },
+                { type: 'maxLength', param: 25 }
+        ]},
+        Name: {
+            type: String,
+            validators: [
+                { type: 'minLength', param: 5 },
+                { type: 'maxLength', param: 100 }
+        ]},
+        Rating: {
+            type: Number,
+            validators: [
+                { type: 'gte', param: 0 },
+                { type: 'lte', param: 10 }
+        ]}
+    }
 });
 
 Meteor.methods({
