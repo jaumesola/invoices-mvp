f224aa665b1156370f7206250f53091919d72120
consert <ask@jaumesola.com>
2017-09-08 12:58:33 +0200

    more on advances

--- imports/advances/advances.func-test.js
+++ imports/advances/advances.func-test.js
@@ -4,20 +4,22 @@ console.log('advances.func-tests');
 import * as th from '../_common/func-test-helpers.js';
 import faker from 'faker';
 
-export const fillForm = function fillForm() {
-    // TODO use consistent values
-    browser.setValue('#OfferId', faker.random.number(100000));
-    browser.setValue('#CreditorId', faker.random.number(100000));
-    browser.setValue('#DebtorId', faker.random.number(100000));
-    browser.setValue('#InvoiceNumber', faker.random.number(100000));
-};
 
 describe('/advances @watch', function() {
     
-  before(function () {
-    browser.url('http://localhost:3000/advances');
-    //server.call('generateFixtures');
-  });
+    var fillFake = function () {
+        // TODO use consistent values
+        browser.setValue('#OfferId', faker.random.number(100000));
+        browser.setValue('#CreditorId', faker.random.number(100000));
+        browser.setValue('#DebtorId', faker.random.number(100000));
+        browser.setValue('#InvoiceNumber', faker.random.number(100000));
+        //altert('filled');
+    };
+  
+    before(function () {
+        browser.url('http://localhost:3000/advances');
+        //server.call('generateFixtures');
+    });
   
   it('click on create button', function() {
       th.waitAndClickFirst('.create');
@@ -28,8 +30,8 @@ describe('/advances @watch', function() {
       chai.assert.equal( browser.getTagName('#InvoiceNumber'), 'input');
   });
   
-  it('add an advance', function () { th.createDoc(fillForm); });
+  it('add an advance', function () { th.createDoc(fillFake); });
   it('select an advance', th.selectDoc);
-  it('edit an advance', function () { th.editDoc(fillForm); });
+  it('edit an advance', function () { th.editDoc(fillFake); });
   it('remove an advance', th.removeDoc);
 });
