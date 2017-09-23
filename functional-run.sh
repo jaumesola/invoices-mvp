99f6216661e37520af6342e921efcef653a78bb3
Jaume Sola <ask@jaumesola.com>
2017-09-23 21:18:34 +0200

    execution scripts moved to cby-deploy sub-project

--- functional-run.sh
+++
@@ -1,5 +0,0 @@
-#
-# usefulness unclear, not yet used....
-#
-set -x
-meteor test --full-app --driver-package tmeasday:acceptance-test-driver
--- unit-tests.sh
+++
@@ -1,2 +0,0 @@
-set -x 
-meteor test --driver-package practicalmeteor:mocha --port 3010
