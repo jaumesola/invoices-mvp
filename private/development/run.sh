0b38845e36c2ae65f64bcbadda52046a6a064a4b
Jaume Sola <ask@jaumesola.com>
2017-10-07 21:16:08 +0200

    run shell script refactoring

--- private/development/run.sh
+++ private/development/run.sh
@@ -1,4 +1,8 @@
 set -x
+export MONGO_URL=mongodb://localhost:3001/meteor
+export MONGO_OPLOG_URL=mongodb://localhost:3001/local
+export CBY_SETTINGS='./private/development/settings.json';
+export CBY_PORT=3000
 
 case $1 in 
 'functional')  # Functional (acceptance) tests with Chimp (files: *.func-tests.*)
--- run-development.sh
+++ run-development.sh
@@ -1,4 +1,1 @@
-set -x
-export CBY_SETTINGS='./private/development/settings.json';
-export CBY_PORT=3000
 ./private/development/run.sh $*
