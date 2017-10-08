942ba67f34b855217b27bf4dcc4f443852ce3d59
consert <ask@jaumesola.com>
2017-10-08 20:46:50 +0200

    fix run.sh so meteor starts mongo

--- private/development/run.sh
+++ private/development/run.sh
@@ -1,6 +1,6 @@
 set -x
-export MONGO_URL=mongodb://localhost:3001/meteor
-export MONGO_OPLOG_URL=mongodb://localhost:3001/local
+//export MONGO_URL=mongodb://localhost:3001/meteor
+//export MONGO_OPLOG_URL=mongodb://localhost:3001/local
 export CBY_SETTINGS='./private/development/settings.json';
 export CBY_PORT=3000
 
